9a84b665bfbbf54d27e07ccab045ea81e72111d8
sittisak  plianklip <sittisak.951753@gmail.com>
2019-12-22 23:50:07 +0700

    make history

--- src/components/about/history.vue
+++ src/components/about/history.vue
@@ -30,14 +30,22 @@
           <h1 class="display-2 font-weight-light">เกี่ยวกับเรา</h1>
         </v-col>
         <v-col cols="12" md="4" sm="4" xs="12" align="center" justify="center">
-            <v-img
+          <v-img
             src="../../assets/img/Picture1.png"
             aspect-ratio="1.3"
             max-width="300"
             max-height="450"
           ></v-img>
         </v-col>
-        <v-col cols="12" md="6" sm="6" xs="12" align="center" justify="center">
+        <v-col cols="12" md="8" sm="8" xs="12">
+          <p>
+            "E-slipamare ถูกสร้างขึ้น โดยมีวัตถุประสงค์
+            เพื่อช่วยในการแก้ไขปัญหาของการเก็บข้อมูลที่อยู่บน E-slip
+            ของธนาคารต่างๆภายในประเทศไทย และยังช่วยให้ผู้ใช้รู้สึกถึงความ
+            ง่ายของการเก็บข้อมูล และยังสามารถใช้ในการเป็นหลักฐาน หรือ
+            ใช้ในการสรุปบัญชีก็ทำได้และยังสรุปได้อย่างรวดเร็วในระยะเวลาอันสั้น"
+          </p>
+          <span class="overline">make by Fluke Tech </span>
         </v-col>
       </v-row>
     </v-container>
